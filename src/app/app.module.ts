fd587e62cda0b7265f5e6485c51038ca7a488fdb
Brendan Ware <bware@mobileiron.com>
2017-08-04 16:02:14 -0700

    Add basic video player

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {VgCoreModule} from 'videogular2/core';
+import {VgControlsModule} from 'videogular2/controls';
+import {VgOverlayPlayModule} from 'videogular2/overlay-play';
+import {VgBufferingModule} from 'videogular2/buffering';
+
 
 import { AppComponent } from './app.component';
-import { TestComponentComponent } from './test-component/test-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    VgCoreModule,
+    VgControlsModule,
+    VgOverlayPlayModule,
+    VgBufferingModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
